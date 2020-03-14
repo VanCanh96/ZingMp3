ae66a0024047c070711d2c367c1e8354c85e7361
đặng cảnh <avancanh@gmail.com>
2020-03-14 20:14:48 +0700

    init

---
+++ Business/Business/Infrastructure/Config.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
+
+namespace Business.Infrastructure
+{
+    public class Config
+    {
+        // scopes define the resources in your system
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("jst_fe", "JST FE", new[] { JwtClaimTypes.Name, JwtClaimTypes.Role, JwtClaimTypes.Email, JwtClaimTypes.Id, JwtClaimTypes.PreferredUserName }),
+            };
+        }
+
+        // clients want to access resources (aka scopes)
+        public static IEnumerable<Client> GetClients()
+        {
+            // client credentials client
+            return new List<Client>
+            {
+                // resource owner password grant client
+                new Client
+                {
+                    ClientId = "jst.ro.client",
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+
+                    ClientSecrets =
+                    {
+                        new Secret("jst.secret".Sha256())
+                    },
+                    AllowedScopes = { "jst_fe", IdentityServerConstants.StandardScopes.OpenId, IdentityServerConstants.StandardScopes.Profile },
+                    AllowOfflineAccess = true,
+                    AccessTokenLifetime = 2592000
+                }
+            };
+        }
+    }
+}
--- Business/Business/Startup.cs
+++ Business/Business/Startup.cs
@@ -1,12 +1,20 @@
 ﻿using Business.Entities;
+using Business.Infrastructure;
 using Business.Repository;
 using Business.Service;
+using FluentMigrator.Runner;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using System.Reflection;
 
 namespace Business
 {
@@ -25,12 +33,36 @@ namespace Business
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
+
+            var builder = // configure identity server with in-memory stores, keys, clients and scopes
+                services.AddIdentityServer()
+                    // this adds the config data from DB (clients, resources)
+                    //.AddProfileService<IdentityProfileService>()
+                    //.AddResourceOwnerValidator<IdentityResourceOwnerPasswordValidator>()
+                    .AddConfigurationStore(options =>
+                    {
+                        options.ConfigureDbContext = b =>
+                            b.UseSqlServer(this.Configuration.GetConnectionString("SqlConnection"),
+                                sql => sql.MigrationsAssembly(migrationsAssembly));
+                    })
+                    // this adds the operational data from DB (codes, tokens, consents)
+                    .AddOperationalStore(options =>
+                    {
+                        options.ConfigureDbContext = b =>
+                            b.UseSqlServer(this.Configuration.GetConnectionString("SqlConnection"),
+                                sql => sql.MigrationsAssembly(migrationsAssembly));
+
+                        // this enables automatic token cleanup. this is optional.
+                        options.EnableTokenCleanup = true;
+                    });
 
             services.AddDbContext<Mp3DbContext>(options =>
               options.UseSqlServer(_connectionString)
@@ -60,6 +92,9 @@ namespace Business
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
+            app.UseIdentityServer();
+            app.UseAuthentication();
+
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
@@ -67,5 +102,45 @@ namespace Business
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
+
+        private void InitializeDatabase(IApplicationBuilder app, IMigrationRunner runner)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in Config.GetClients())
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in Config.GetIdentityResources())
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiResources.Any())
+                {
+                    foreach (var resource in Config.GetApiResources())
+                    {
+                        context.ApiResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+            }
+
+            runner.MigrateUp();
+        }
+
     }
 }
